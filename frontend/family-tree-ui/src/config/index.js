db1248a4181703105c2ff4979ec7d25f086d0361
Michael Nunez <nunezmichaelsp@gmail.com>
2025-03-13 13:52:31 -0600

    Configure prod urls.

--- frontend/family-tree-ui/src/config/index.js
+++ frontend/family-tree-ui/src/config/index.js
@@ -4,6 +4,6 @@ export const Config = {
   API_URL:
     ENV === "development"
       ? "http://localhost:5118/api" // Dev Backend
-      : "https://your-backend-production-url.com/api", // Prd Backend
+      : "https://familytreeapi-cpc9g4gca5gdhuc7.centralus-01.azurewebsites.net/api", // Prd Backend
   CLIENT_ID: "my-constant-client-id",
 };
